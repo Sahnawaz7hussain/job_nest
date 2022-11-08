46baec402dc46de1b12e15ae614deba165bae384
Sahnawaz Hussain <sahnawazhussain852@gmail.com>
2022-11-08 17:56:09 +0530

    commponents&assests&utils

---
+++ src/Components/Dummyfile.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Dummyfile = () => {
+  return <div>Dummyfile</div>;
+};
+
+export default Dummyfile;
---
+++ src/utils/localData.js
@@ -0,0 +1,14 @@
+const saveToLocalStorage = (key, value) => {
+  localStorage.setItem(key, JSON.stringify(value));
+};
+
+const getLocalStorageData = (key) => {
+  let data = JSON.parse(localStorage.getItem(key));
+  return data;
+};
+
+const removeLocalStorageData = (key) => {
+  localStorage.removeItem(key);
+};
+
+export { saveToLocalStorage, getLocalStorageData, removeLocalStorageData };
