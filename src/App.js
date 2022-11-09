2c113515586fda1757d2d7f09e2a61fc7aa3c5eb
Sahnawaz Hussain <sahnawazhussain852@gmail.com>
2022-11-09 21:49:13 +0530

    mered

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import Companies from "./Pages/Companies";
+import Searchbar from "./Components/Searchbar";
+import TypesOfJob from "./Components/TypesOfJob";
 
 function App() {
   return (
     <div className="App">
       <Companies />
+      <Searchbar />
+      <TypesOfJob />
     </div>
   );
 }
