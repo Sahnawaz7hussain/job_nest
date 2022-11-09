d3f455b950f4cb1eccd3a817aacd656b3b1d752a
martina024 <107464455+martina024@users.noreply.github.com>
2022-11-09 19:09:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <h2>Naukri.com</h2>
+    <Navbar/>
     </div>
   );
 }
