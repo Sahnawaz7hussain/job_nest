452b61df9379238f92939d7e0efb82c048745da1
martina024 <107464455+martina024@users.noreply.github.com>
2022-11-09 22:57:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import "./App.css";
-import Navbar from "./Components/Navbar";
-
+import Companies from "./Pages/Companies";
+import Searchbar from "./Components/Searchbar";
+import TypesOfJob from "./Components/TypesOfJob";
+import Dummy from "./Components/Dummy2";
+import Navbar from "./Components/Navbar"
 function App() {
   return (
     <div className="App">
-      <h2>Naukri.com</h2>
-    <Navbar/>
+      <Navbar/>
+      <Companies />
+      <Searchbar />
+      <TypesOfJob />
+      <Dummy />
     </div>
   );
 }
 
 export default App;
+
