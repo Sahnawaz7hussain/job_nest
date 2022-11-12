f9b5bd12261bffdd502a87348c495e769965eca6
Sahnawaz Hussain <sahnawazhussain852@gmail.com>
2022-11-12 17:05:02 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-
 import "./App.css";
 import React from "react";
-import "./App.css";
-import Navbar from "./Components/Navbar";
-
-import './App.css';
-import React from 'react';
-// import Navbar from "./Components/Navbar";
 import MainRoute from "./Pages/MainRoute";
 
 function App() {
   return (
     <div className="App">
-
-      
-
-      {/* <Navbar /> */}
-
       <MainRoute />
     </div>
   );
